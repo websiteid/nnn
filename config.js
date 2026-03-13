ac4188b645e97923db442d8bd522ba37d6900123
websiteid <junimatalgano@gmail.com>
2026-03-13 08:45:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,5 @@
-
+module.exports = {
+  BOT_TOKEN: '7524016177:AAEAQHIF4TvD--t5Bbr8Rb6MjdrBzUcd5Q0',
+  CHANNEL_ID: '-1002857800900', // Bisa pakai @username atau ID angka
+  ADMIN_ID: '6468926488'                // ID Telegram Admin (angka)
+};
